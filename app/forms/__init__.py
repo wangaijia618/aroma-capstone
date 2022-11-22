e6840326ea17a467756251edf98f64931cc5e11a
Aijia Wang <ajw7427@gmail.com>
2022-11-22 11:46:43 -0600

    favicon/stylesheet/ fontawesome

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,5 +1,5 @@
-# from .login_form import LoginForm
-# from .signup_form import SignUpForm
-# from .comment_form import CommentForm
-# from .profile_form import ProfileForm
-# from .story_form import StoryForm
+from .login_form import LoginForm
+from .signup_form import SignUpForm
+from .comment_form import CommentForm
+from .profile_form import ProfileForm
+from .story_form import StoryForm
